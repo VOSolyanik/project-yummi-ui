124292da4731dfa341b89325d390f3de57742469
nellipestych <NelliPestych@bigmir.net>
2025-09-27 22:22:59 +0300

    created block recipes, fix styles

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -30,28 +30,15 @@ const RecipeList = ({ recipes, onFavoriteToggle, isLoading, error }) => {
     );
   }
 
-  const renderRecipesInRows = () => {
-    const rows = [];
-    for (let i = 0; i < recipes.length; i += 3) {
-      const rowRecipes = recipes.slice(i, i + 3);
-      rows.push(
-        <div key={i} className={css.row}>
-          {rowRecipes.map((recipe) => (
-            <RecipeCard
-              key={recipe.id}
-              recipe={recipe}
-              onFavoriteToggle={onFavoriteToggle}
-            />
-          ))}
-        </div>
-      );
-    }
-    return rows;
-  };
-
   return (
     <div className={css.grid}>
-      {renderRecipesInRows()}
+      {recipes.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          recipe={recipe}
+          onFavoriteToggle={onFavoriteToggle}
+        />
+      ))}
     </div>
   );
 };
