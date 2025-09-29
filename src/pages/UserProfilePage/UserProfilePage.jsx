dec615632040bf27bd8c0176c0eda22d43d85c05
julia <julia.zhyvylo@isolutions.mk>
2025-09-29 21:10:39 +0200

    add log out btn

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import css from './UserProfilePage.module.css';
+
+import Button from '@/components/Button/Button';
 import { UserInfoCard } from '@/components/UserInfoCard/UserInfoCard';
 
 
 export const UserProfilePage = () => {
-  return <UserInfoCard></UserInfoCard>;
+  return (
+    <>
+      <div>
+        <UserInfoCard />
+        <Button className={css.btn}>LOG OUT</Button>
+      </div>
+    </>
+  );
 };
