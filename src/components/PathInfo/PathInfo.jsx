fe44ea0ab6cedcf5d6e44b39ce14acdce7fea4ea
Oleksandr Knyaz <lichknyaz@gmail.com>
2025-10-05 14:08:53 +0200

    fix styles

--- src/components/PathInfo/PathInfo.jsx
+++ src/components/PathInfo/PathInfo.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import clsx from 'clsx';
+
 import css from './PathInfo.module.css';
 
-const PathInfo = ({ currentPage }) => {
+const PathInfo = ({ currentPage, className }) => {
   return (
-    <div className={css.pathInfo}>
+    <div className={clsx(css.pathInfo, className)}>
       <Link to="/" className={css.link}>Home</Link>
       <span className={css.separator}>/</span>
       <span className={css.currentPage}>{currentPage}</span>
